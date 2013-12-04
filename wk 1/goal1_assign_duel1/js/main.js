7a56319d0a1e2577cef2e5e1c9a743cf8544e2f3
Alicia Zegelien <aliciazegelien@unknowna8bbcf0e8918.att.net>
2013-12-03 22:54:22 -0500

    Goal 1 the Duel part  I
    doing the follow along and this is the variables used

---
+++ wk 1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,24 @@
+// Name: Alicia Zegelien
+// Date: 12-3-13
+// Assignment: Goal 1: The Duel - Part I
+
+
+(function(){
+
+console.log("fight");
+
+//Player Names
+var playerOneName = "Spiderman";
+var playerTwoName = "Batman";
+
+//player Damage
+var player1Damage = 20;
+var player2Damage = 20;
+
+//player Health
+var playerOneHealth = 100;
+var playerTwoHealth = 100;
+
+var round = 1;
+
+})();
