c9913b6d0e0518eb3c39ca7b7acb1a706d3c4d6b
Alicia Zegelien <aliciazegelien@unknowna8bbcf0e8918.att.net>
2013-12-03 23:13:17 -0500

    added fight function and For loop. also added min damage for players 1 and 2

--- wk 1/goal1_assign_duel1/js/main.js
+++ wk 1/goal1_assign_duel1/js/main.js
@@ -5,20 +5,44 @@
 
 (function(){
 
-console.log("fight");
+	console.log("fight");
 
-//Player Names
-var playerOneName = "Spiderman";
-var playerTwoName = "Batman";
+	//Player Names
+	var playerOneName = "Spiderman";
+	var playerTwoName = "Batman";
 
-//player Damage
-var player1Damage = 20;
-var player2Damage = 20;
+	//player Damage
+	var player1Damage = 20;
+	var player2Damage = 20;
 
-//player Health
-var playerOneHealth = 100;
-var playerTwoHealth = 100;
+	//player Health
+	var playerOneHealth = 100;
+	var playerTwoHealth = 100;
 
-var round = 1;
+	var round = 1;
+
+	function fight(){
+		console.log("In the fight function");
+
+		alert(playerOneName+":"+playerONeHealth+" *START* "+playerTwoName+":"+playerTwoHealth);
+
+
+		for(var i=0; i<10; i++){
+			 
+			 var minDamage1 = player1Damage * .5;
+			 var minDamage2 = player2Damage * .5;
+
+		};
+
+		
+	};
+
+	function winnerCheck(){
+
+	};
+
+	
+   	console.log("program starts");
+	fight();
 
 })();
