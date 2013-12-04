e8fe3cb5e00543ab9af09e87bf567a70ce64d08b
Alicia Zegelien <aliciazegelien@unknowna8bbcf0e8918.att.net>
2013-12-03 23:48:17 -0500

    Finished the follow along. check my work several times.  this commit added alerts and code in the functions

--- wk 1/goal1_assign_duel1/js/main.js
+++ wk 1/goal1_assign_duel1/js/main.js
@@ -19,18 +19,41 @@
 	var playerOneHealth = 100;
 	var playerTwoHealth = 100;
 
-	var round = 1;
+	var round = 0;
 
 	function fight(){
 		console.log("In the fight function");
 
-		alert(playerOneName+":"+playerONeHealth+" *START* "+playerTwoName+":"+playerTwoHealth);
+		alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth);
 
 
 		for(var i=0; i<10; i++){
 			 
 			 var minDamage1 = player1Damage * .5;
 			 var minDamage2 = player2Damage * .5;
+			 var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+			 var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+			 //inflict Damage
+			 playerOneHealth-=f1;
+			 playerTwoHealth-=f2;
+
+			 console.log(playerOneName+":"+playerOneHealth+" "+playerTwoName+":"+playerTwoHealth);
+
+			 var results = winnerCheck();
+
+			 console.log(results);
+
+			 if (results ==="no winner"){
+			 	round++;
+			 	alert(playerOneName+":"+playerOneHealth+ " ROUND "+round+" OVER "+playerTwoName+":"+playerTwoHealth);
+
+			 }else{
+			 	alert(results);
+			 	break;
+			 };
+
+
 
 		};
 
@@ -38,6 +61,20 @@
 	};
 
 	function winnerCheck(){
+		console.log("in winnerCheck FN");
+		var result="no winner";
+
+		if(playerOneHealth <1 && playerTwoHealth <1){
+
+			result="You Both Die";
+
+		}else if(playerOneHealth<1){
+			result =PlayerTwoName + " Wins!!!";
+
+		}else if(playerTwoHealth<1){
+			result = playerOneName+ " Wins!!!";
+		};
+		return result;
 
 	};
 
